98101f3806fff4f94055b14641e39e1a7772d9e9
Gaurav Paruthi <gauravparuthi@Gauravs-MacBook-Pro.local>
2016-05-11 01:11:22 -0400

    updated

--- app/js/mixins.js
+++ app/js/mixins.js
@@ -1,31 +1,14 @@
 OptsMixin = {
-  // init method is a special one which can initialize
-  // the mixin when it's loaded to the tag and is not
-  // accessible from the tag its mixed in
-  // init: function() {
-  //   this.on('updated', function() { console.log(this.root.tagName) })
-  // },
-
-  // getOpts: function() {
-  //   return this.opts
-  // },
-
-  // setOpts: function(opts, update) {
-  //   this.opts = opts
-  //   if (!update) this.update()
-  //   return this
-  // },
-    track: function(arg='default', arg2="") {
-    	var tagName = this.root.tagName
-    	fbq('trackCustom', tagName , {action: arg});
+    track: function(arg, arg2) {
+    	arg = arg || 'default';
+    	arg2 = arg2 || '';
+    	var tagName = this.root.tagName;
+    	fbq('trackCustom', tagName , {"action": arg});
     	ga('send', {
-		  hitType: 'event',
-		  eventCategory: tagName,
-		  eventAction: arg,
-		  eventLabel: arg2,
+		  "hitType": 'event',
+		  "eventCategory": tagName,
+		  "eventAction": arg,
+		  "eventLabel": arg2,
 		});
   }
 }
-
-
-
--- gulp/config/compile/tag.js
+++ gulp/config/compile/tag.js
@@ -1,13 +1,15 @@
 var gulp = require('gulp');
+
+var gulp_riot = require('gulp-riot');
+
 var $ = require('gulp-load-plugins')();
+
 
 gulp.task('riot', [ 'fonts'], function () {
   var lazypipe = require('lazypipe');
   
 
-  return gulp.src(['app/**/*.tag'])
-    
-    // .pipe($.if('*.js', $.uglify()))
-    .pipe($.useref())
+  return gulp.src(['app/components/*.tag'])
+    .pipe(gulp_riot.riot())
     .pipe(gulp.dest('dist'));
 });
