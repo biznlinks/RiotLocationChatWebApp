83576c69a202b6aeea13b390336f72c5ab557f55
Gaurav Paruthi <gparuthi@umich.edu>
2016-05-17 21:05:36 -0400

    updated

--- dist/js/mixins.js
+++ dist/js/mixins.js
@@ -1,1 +1,14 @@
-OptsMixin = { track: function(arg, arg2) { arg = arg || 'default'; arg2 = arg2 || ''; var tagName = this.root.tagName; fbq('trackCustom', tagName , {"action": arg}); ga('send', { "hitType": 'event', "eventCategory": tagName, "eventAction": arg, "eventLabel": arg2, }); } }
+OptsMixin = {
+    track: function(arg, arg2) {
+    	arg = arg || 'default';
+    	arg2 = arg2 || '';
+    	var tagName = this.root.tagName;
+    	fbq('trackCustom', tagName , {"action": arg});
+    	ga('send', {
+		  "hitType": 'event',
+		  "eventCategory": tagName,
+		  "eventAction": arg,
+		  "eventLabel": arg2,
+		});
+  }
+}
