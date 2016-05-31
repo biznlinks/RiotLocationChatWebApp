82d7c83099dc0e2a29c5c7424248fb156da0b1fe
Gaurav Paruthi <gparuthi@umich.edu>
2016-05-31 13:03:12 -0700

    Deleted npm and styling

--- dist/js/mixins.js
+++
@@ -1,1 +0,0 @@
-OptsMixin = { track: function(arg, arg2) { arg = arg || 'default'; arg2 = arg2 || ''; var tagName = this.root.tagName; fbq('trackCustom', tagName , {"action": arg}); ga('send', { "hitType": 'event', "eventCategory": tagName, "eventAction": arg, "eventLabel": arg2, }); } }
