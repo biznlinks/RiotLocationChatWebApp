47bb07fdd06c12fdf147b1e814b7a2fb04b147a4
Gaurav Paruthi <gparuthi@umich.edu>
2016-05-31 17:27:38 -0700

    fixed post with handle

--- app/js/init.js
+++ app/js/init.js
@@ -1,13 +1,4 @@
-// // chi
-            // Parse.initialize("NV3PXWz0D6qx90BI1aiWKY1gR8IJ2aBS07jeN566", "QHfiiv5CQkTVNH9Bpj3iMntZJ2ze3L8veDlj912R");
 
- // ictd
- Parse.initialize("03XBJy9KH27oXnjBDsV7noOSJ4wHTkbRd78XCODg", "Gi8T8CK2XULruSKx71LyY9PHtptBUJk6Jz0ktH1S");
- FBAppId = '822486861133305';
-
- // ictd-devo
-  // Parse.initialize("YDTZ5PlTlCy5pkxIUSd2S0RWareDqoaSqbnmNX11", "1KFxPXKErTsVAe5Zu9Gx5SDS40qNV6WxmVQ5gUuB");
-  // FBAppId = '399505526886852';
 
 // old-databse
 //Parse.initialize("Rw6U09ERS7r2VAiPbqXV8iSDTuhOoAYJqzbRa0Cs", "6PxPrGuIqchYYdjPyCiHccjCbMKjSAfmAHyFwjEr");
